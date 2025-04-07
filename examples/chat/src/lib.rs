ab504960892550e7e1e9b58ce338381ca8ff4ac7
verbiiyo <dyllanmccreary@gmail.com>
2025-04-07 19:11:58 +0900

    ok a much smaller error log

--- examples/chat/src/lib.rs
+++ examples/chat/src/lib.rs
@@ -1,6 +1,3 @@
-//! Minimal Gossipsub P2P Chat - Rust WASM (browser-only)
-
-// lib.rs
 #![cfg(target_arch = "wasm32")]
 
 use std::collections::hash_map::DefaultHasher;
@@ -8,8 +5,7 @@ use std::hash::{Hash, Hasher};
 use std::time::Duration;
 
 use futures::StreamExt;
-use js_sys::Date;
-use libp2p::{core::Multiaddr, gossipsub, swarm::SwarmEvent};
+use libp2p::{gossipsub, swarm::SwarmEvent};
 use libp2p_webrtc_websys as webrtc_websys;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
@@ -39,10 +35,8 @@ async fn run() -> Result<(), JsError> {
     tracing_wasm::set_as_global_default();
     let body = Body::from_current_window()?;
 
-    // Gossipsub topic
     let topic = gossipsub::IdentTopic::new("xos-demo");
 
-    // Build swarm
     let message_id_fn = |message: &gossipsub::Message| {
         let mut s = DefaultHasher::new();
         message.data.hash(&mut s);
@@ -59,7 +53,7 @@ async fn run() -> Result<(), JsError> {
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_wasm_bindgen()
         .with_other_transport(|key| {
-            webrtc_websys::Transport::new(webrtc_websys::Config::new(&key))
+            Ok(webrtc_websys::Transport::new(webrtc_websys::Config::new(&key)))
         })?
         .with_behaviour(|key| {
             let gs = gossipsub::Behaviour::new(
@@ -75,11 +69,12 @@ async fn run() -> Result<(), JsError> {
     let (tx, mut rx): (UnboundedSender<String>, UnboundedReceiver<String>) = unbounded();
     *MSG_TX.lock().unwrap() = Some(tx);
 
-    // Broadcast loop
-    let mut swarm2 = swarm.clone();
+    // Broadcast loop (no clone)
+    let tx_topic = topic.clone();
+    let tx_swarm = &mut swarm;
     spawn_local(async move {
         while let Some(msg) = rx.next().await {
-            let _ = swarm2.behaviour_mut().publish(topic.clone(), msg.as_bytes());
+            let _ = tx_swarm.behaviour_mut().publish(tx_topic.clone(), msg.as_bytes());
         }
     });
 
@@ -88,7 +83,7 @@ async fn run() -> Result<(), JsError> {
         match swarm.next().await.unwrap() {
             SwarmEvent::Behaviour(gossipsub::Event::Message { message, .. }) => {
                 let text = String::from_utf8_lossy(&message.data);
-                body.append_p(&format!("{}", text))?;
+                body.append_p(&text)?;
             }
             _ => {}
         }
@@ -109,9 +104,9 @@ impl Body {
     }
 
     fn append_p(&self, msg: &str) -> Result<(), JsError> {
-        let val = self.document.create_element("p")?;
+        let val = self.document.create_element("p").map_err(js_error)?;
         val.set_text_content(Some(msg));
-        self.body.append_child(&val)?;
+        self.body.append_child(&val).map_err(js_error)?;
         Ok(())
     }
 }
